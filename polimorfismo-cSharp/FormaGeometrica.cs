a6a34bdaea1d5b8bf83c9c7cff1effc9e9fe61e0
Rodrigo Seliprin <rseliprin@gmail.com>
2023-04-30 08:09:01 -0300

    Adicionar arquivos de projeto.

---
+++ polimorfismo-cSharp/FormaGeometrica.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polimorfismo_cSharp
+{
+    public class FormaGeometrica
+    {
+        public virtual void Desenhar()
+        {
+
+        }
+    }
+
+    public class Circulo : FormaGeometrica
+    {
+        public override void Desenhar()
+        {
+            Console.WriteLine("Desenhando um círculo!");
+        }
+    }
+
+    public class Quadrado : FormaGeometrica
+    {
+        public override void Desenhar()
+        {
+            Console.WriteLine("Desenhando um quadrado!");
+        }
+    }
+}
---
+++ polimorfismo-cSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using polimorfismo_cSharp;
+
+List<FormaGeometrica> formas = new List<FormaGeometrica>
+    {
+        new Circulo(),
+        new Quadrado()
+    };
+
+foreach (FormaGeometrica forma in formas)
+{
+    forma.Desenhar();
+}
